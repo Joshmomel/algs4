051f9e4c637b98181705ba5bab794451e16eba18
Josh Mo <joshmomel@gmail.com>
2022-09-28 08:12:13 +1000

    feat: adding skeleton for Heap sort

---
+++ src/heap/Heap.java
@@ -0,0 +1,20 @@
+package heap;
+
+public class Heap {
+  public static void sort(Comparable[] a) {
+
+  }
+
+  private static void sink(Comparable[] a, int k, int N) {
+
+  }
+
+  private static boolean less(Comparable[] a, int i, int j) {
+    return false;
+  }
+
+  private static void exch(Comparable[] a, int i, int j) {
+
+  }
+
+}
